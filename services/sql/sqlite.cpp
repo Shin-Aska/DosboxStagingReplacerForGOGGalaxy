dd72cdeffcff6ac5d50d4e4376263541ff34783a
Richard Louie Orilla <richard.orilla@inriver.com>
2025-03-23 19:13:27 +0800

    Add initial implementation of SqlLiteService and header files

---
+++ services/sql/sqlite.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Orill on 3/23/2025.
+//
+
+#ifndef SQLITE_CPP
+#define SQLITE_CPP
+
+#include "sqlite.h"
+
+namespace DosboxStagingReplacer {
+} // DosboxStagingReplacer
+
+#endif // SQLITE_CPP
---
+++ services/sql/sqlite.h
@@ -0,0 +1,16 @@
+//
+// Created by Orill on 3/23/2025.
+//
+
+#ifndef SQLITE_H
+#define SQLITE_H
+
+namespace DosboxStagingReplacer {
+
+class SqlLiteService {
+
+};
+
+} // DosboxStagingReplacer
+
+#endif //SQLITE_H
