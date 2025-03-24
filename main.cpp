e574104d2d2fcf20ffa7c118fcf0a5a29a5ed8dd
Richard Louie Orilla <richard.orilla@inriver.com>
2025-03-24 17:09:40 +0800

    Update database version in main.cpp and display product list

--- main.cpp
+++ main.cpp
@@ -11,8 +11,13 @@ using namespace DosboxStagingReplacer;
 
 int main(int argc, char *argv[]) {
 
-    auto service = GogGalaxyService("galaxy-2.1.db");
+    auto service = GogGalaxyService("galaxy-2.0.db");
     std::cout << "Database is valid: " << service.isDatabaseValid() << std::endl;
+
+    std::cout << "Products:" << std::endl;
+    for (const auto &product : service.getProducts()) {
+        std::cout << product.title << " (" << product.productId << ")" << std::endl;
+    }
 
     return 0;
 }
