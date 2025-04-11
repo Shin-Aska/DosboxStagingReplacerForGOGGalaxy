090d7047de7290bc128ae8df071831471a2f0d01
Richard Louie Orilla <richard.orilla@inriver.com>
2025-04-12 01:38:27 +0800

    Add DataExporter integration in main.cpp for application and game listing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
-#include "GoGService.h"
+#include "DataExporter.h"
 #include "FileBackupService.h"
+#include "StatementParser.h"
+#include "GoGService.h"
 #include "helpers/finders/InstallationFinder.h"
 #include "helpers/scanners/DirectoryScanner.h"
 #include "helpers/verifiers/InstallationVerifier.h"
@@ -12,8 +14,8 @@
 
 int main(int argc, char *argv[]) {
 
-    DosboxStagingReplacer::DirectoryScanner scanner;
     DosboxStagingReplacer::FileBackupService fileBackupService;
+    DosboxStagingReplacer::DataExporter dataExporter;
 
     argparse::ArgumentParser program("Dosbox Staging Replacer");
     program.add_argument("-f", "--file")
@@ -28,22 +30,22 @@ int main(int argc, char *argv[]) {
         .help("Create a backup of the Galaxy database")
         .default_value(false)
         .implicit_value(true)
-        .nargs(1);
+        .nargs(0);
     program.add_argument("-r", "--restore")
         .help("Restore the backup of the Galaxy database")
         .default_value(false)
         .implicit_value(true)
-        .nargs(1);
+        .nargs(0);
     program.add_argument("-la", "--list-applications")
         .help("Print all installed applications")
         .default_value(false)
         .implicit_value(true)
-        .nargs(1);
+        .nargs(0);
     program.add_argument("-lg", "--list-games")
         .help("Print all installed games")
         .default_value(false)
         .implicit_value(true)
-        .nargs(1);
+        .nargs(0);
     program.add_argument("-fmt", "--format")
         .help("The format used when doing any print operation")
         .default_value(std::string(".json"))
@@ -73,10 +75,8 @@ int main(int argc, char *argv[]) {
     // }
 
     try {
+        DosboxStagingReplacer::DirectoryScanner scanner;
         files = scanner.scanDirectory(chosenPath);
-        for (const auto &file : files) {
-            std::cout << file.name << " (" << file.path << ")" << std::endl;
-        }
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return -1;
@@ -104,12 +104,18 @@ int main(int argc, char *argv[]) {
     }
     else if (program["--list-applications"] == true) {
         auto applications = DosboxStagingReplacer::getInstalledApplications();
-        for (const auto &application : applications) {
-            std::cout << application.applicationName << " (" << application.installationPath << ")" << std::endl;
-        }
+        std::cout << dataExporter.serialize(applications) << std::endl;
     }
     else if (program["--list-games"] == true) {
-        auto games = service.getProducts();
+        // Need to convert the result of getProducts to a vector of shared pointers so we can
+        // take advantage of polymorphism
+        std::vector<std::shared_ptr<DosboxStagingReplacer::SqlDataResult>> games;
+        service.openConnection(chosenPath + "\\" + chosenFile);
+        for (auto& product : service.getProducts()) {
+            games.push_back(std::make_shared<DosboxStagingReplacer::ProductDetails>(product));
+        }
+        std::cout << dataExporter.serialize(games) << std::endl;
+        service.closeConnection();
     }
 
     return 0;
