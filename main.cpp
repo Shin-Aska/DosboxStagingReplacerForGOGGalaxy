ab1bd87d7c25160964ddd1aaa7eb4c8f965080b3
Richard Louie Orilla <richard.orilla@inriver.com>
2025-04-12 00:04:12 +0800

    Add FileBackupService integration and enhance main.cpp with backup/restore functionality

--- main.cpp
+++ main.cpp
@@ -2,46 +2,115 @@
 #include <string>
 
 #include "GoGService.h"
+#include "FileBackupService.h"
 #include "helpers/finders/InstallationFinder.h"
 #include "helpers/scanners/DirectoryScanner.h"
 #include "helpers/verifiers/InstallationVerifier.h"
+
+#include "libs/argparse/argparse.hpp"
 
 
 int main(int argc, char *argv[]) {
 
     DosboxStagingReplacer::DirectoryScanner scanner;
-    DosboxStagingReplacer::GogGalaxyService service("galaxy-2.0.db");
+    DosboxStagingReplacer::FileBackupService fileBackupService;
 
-    // Environment specific variables
-#ifdef __linux__
-    std::string path = "/home/richard/Downloads";
-    std::string existingFile = "/home/richard/Downloads/install-vs-code.bat";
-    std::string nonExistingFile = "/home/richard/Downloads/superwave.wsz2";
-#else
-    std::string path = "C:\\Users\\Orill\\Downloads";
-    std::string existingFile = "C:\\Users\\Orill\\Downloads\\instspeedfan452.exe";
-    std::string nonExistingFile = "C:\\Users\\Orill\\Downloads\\superwave.wsz2";
-#endif
+    argparse::ArgumentParser program("Dosbox Staging Replacer");
+    program.add_argument("-f", "--file")
+        .help("The Galaxy database file")
+        .default_value(std::string("galaxy-2.0.db"))
+        .nargs(1);
+    program.add_argument("-d", "--directory")
+        .help("The GoG Galaxy storage directory")
+        .default_value(std::string(R"(C:\ProgramData\GOG.com\Galaxy\storage)"))
+        .nargs(1);
+    program.add_argument("-b", "--backup")
+        .help("Create a backup of the Galaxy database")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(1);
+    program.add_argument("-r", "--restore")
+        .help("Restore the backup of the Galaxy database")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(1);
+    program.add_argument("-la", "--list-applications")
+        .help("Print all installed applications")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(1);
+    program.add_argument("-lg", "--list-games")
+        .help("Print all installed games")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(1);
+    program.add_argument("-fmt", "--format")
+        .help("The format used when doing any print operation")
+        .default_value(std::string(".json"))
+        .nargs(1);
+    program.add_argument("-o", "--output")
+        .help("The name of the output file. If \"\", the output will be printed to the console.")
+        .default_value(std::string(""))
+        .nargs(1);
 
-    auto files = scanner.scanDirectory(path);
-    for (const auto &file : files) {
-        std::cout << file.name << " (" << file.path << ")" << std::endl;
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
     }
 
-    auto applications = DosboxStagingReplacer::getInstalledApplications();
-    for (const auto &application : applications) {
-        std::cout << application.applicationName << " (" << application.installationPath << ")" << std::endl;
+    const auto chosenFile = program.get<std::string>("--file");
+    const auto chosenPath = program.get<std::string>("--directory");
+    std::vector<DosboxStagingReplacer::FileEntity> files;
+    DosboxStagingReplacer::GogGalaxyService service;
+
+    // auto applications = DosboxStagingReplacer::getInstalledApplications();
+    // for (const auto &application : applications) {
+    //     std::cout << application.applicationName << " (" << application.installationPath << ")" << std::endl;
+    // }
+
+    try {
+        files = scanner.scanDirectory(chosenPath);
+        for (const auto &file : files) {
+            std::cout << file.name << " (" << file.path << ")" << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return -1;
     }
 
-    std::cout << existingFile << " Exists? " <<  (DosboxStagingReplacer::fileExists(existingFile) == true ? "Yes" : "No") << std::endl;
-    std::cout << nonExistingFile << " Exists? " <<  (DosboxStagingReplacer::fileExists(nonExistingFile) == true ? "Yes" : "No") << std::endl;
-
-    std::cout << "Is Database Valid? " << (service.isDatabaseValid() == true ? "Yes" : "No") << std::endl;
-
-    // Get all products
-    auto products = service.getProducts();
-    for (const auto &product : products) {
-        std::cout << product.gogId << " (" << product.releaseKey << ")" << std::endl;
+    if (program["--backup"] == true) {
+        service.openConnection(chosenPath + "\\" + chosenFile);
+        if (!service.isDatabaseValid()) {
+            std::cerr << "Error: The database is not valid. Quitting program" << std::endl;
+            return -1;
+        }
+        std::cout << "Creating a backup of " << chosenFile << " in " << chosenPath << std::endl;
+        service.closeConnection();
+        fileBackupService.createBackup(chosenPath + "\\" + chosenFile, files);
     }
+    else if (program["--restore"] == true) {
+        service.openConnection(chosenPath + "\\" + chosenFile);
+        if (!service.isDatabaseValid()) {
+            std::cerr << "Error: The database is not valid. Quitting program" << std::endl;
+            return -1;
+        }
+        std::cout << "Restoring the backup of " << chosenFile << " in " << chosenPath << std::endl;
+        service.closeConnection();
+        fileBackupService.restoreFromBackup(chosenPath + "\\" + chosenFile, files);
+    }
+    else if (program["--list-applications"] == true) {
+        auto applications = DosboxStagingReplacer::getInstalledApplications();
+        for (const auto &application : applications) {
+            std::cout << application.applicationName << " (" << application.installationPath << ")" << std::endl;
+        }
+    }
+    else if (program["--list-games"] == true) {
+        auto games = service.getProducts();
+    }
+
     return 0;
 }
