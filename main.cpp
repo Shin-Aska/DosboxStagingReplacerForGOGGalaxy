7ab2c0ed6d33a362de63c858b522e61ce0b34cdd
Richard Louie Orilla <richard.orilla@inriver.com>
2025-04-11 12:56:40 +0800

    Update file path in main.cpp and specify SqliteSchema type in executeQuery call

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
     std::string nonExistingFile = "/home/richard/Downloads/superwave.wsz2";
 #else
     std::string path = "C:\\Users\\Orill\\Downloads";
-    std::string existingFile = "C:\\Users\\Orill\\Downloads\\superwave.wsz";
+    std::string existingFile = "C:\\Users\\Orill\\Downloads\\instspeedfan452.exe";
     std::string nonExistingFile = "C:\\Users\\Orill\\Downloads\\superwave.wsz2";
 #endif
 
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Is SqliteService connected? " << (service.isConnectionOpen() == true ? "Yes" : "No") << std::endl;
 
-    auto result = service.executeQuery<SqliteSchema>(R"SQL(
+    auto result = service.executeQuery<DosboxStagingReplacer::SqliteSchema>(R"SQL(
         SELECT
             type,
             name,
