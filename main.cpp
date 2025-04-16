32f0409f06114322a71b8193840131fe4a851b5a
Richard Louie Orilla <richard.orilla@inriver.com>
2025-04-16 18:08:26 +0800

    Add command-line options for DOSBox management and play task retrieval

--- main.cpp
+++ main.cpp
@@ -53,8 +53,18 @@ int main(int argc, char *argv[]) {
         .default_value(false)
         .implicit_value(true)
         .nargs(0);
+    program.add_argument("-do", "--dos-only")
+        .help("This flag signifies that any list operations will only be DOS related.")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(0);
     program.add_argument("-lg", "--list-games")
         .help("Print all installed games")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(0);
+    program.add_argument("-sp", "--show-playtasks")
+        .help("Shows all play task registered for a game")
         .default_value(false)
         .implicit_value(true)
         .nargs(0);
@@ -63,17 +73,29 @@ int main(int argc, char *argv[]) {
         .default_value(false)
         .implicit_value(true)
         .nargs(0);
+
+    std::unordered_map <std::string, std::string> dosBoxVersionParameters = {
+        {"dosbox-staging", "DOSBox Staging"},
+        {"dosbox-x", "DOSBox-X"},
+        {"dosbox-ece", "DOSBox ECE"}
+    };
     program.add_argument("-dv", "--dosbox-version")
         .help("The version of DOSBox to use. This is a string, the format is the application name that is "
               "printed by the --list-applications flag. When using this flag, the version of DOSBox you want to "
               "use must be installed in the system.")
         .default_value(std::string(""))
-        .nargs('?');
+        .choices("", "dosbox-staging", "dosbox-x", "dosbox-ece")
+        .nargs(1);
+
     program.add_argument("-dvm", "--dosbox-version-manual")
         .help("The version of DOSBox to use. This is a string containing the absolute path to the DOSBox executable. "
               "Use this if you want to use a version of DOSBox that is not installed in the system or is a portable executable.")
         .default_value(std::string(""))
-        .nargs('?');
+        .nargs(1);
+    program.add_argument("-rk", "--release-key")
+        .help("The release key of the selected game. You can get this from the --list-games flag. ")
+        .default_value(std::string(""))
+        .nargs(1);
     program.add_argument("-fmt", "--format")
         .help("The format used when doing any print operation")
         .default_value(std::string(".json"))
@@ -101,6 +123,7 @@ int main(int argc, char *argv[]) {
             program["--list-applications"] == true,
             program["--list-games"] == true,
             program["--replace-dosbox"] == true,
+            program["--show-playtasks"] == true
     };
 
     const auto operations_count = std::ranges::count(flags, true);
@@ -121,6 +144,17 @@ int main(int argc, char *argv[]) {
         !program.get<std::string>("--dosbox-version-manual").empty()
     };
 
+    // If the show playtask flag is set to true, we check if the --release-key flag has a value provided, otherwise
+    // we print an error message and exit
+
+    const auto releaseKey = program.get<std::string>("--release-key");
+    if (program["--show-playtasks"] == true) {
+        if (releaseKey.empty()) {
+            std::cerr << "Error: You must supply a value for --release-key" << std::endl;
+            return -1;
+        }
+    }
+
     if (program["--replace-dosbox"] == true) {
         if (const auto count = std::ranges::count(replaceDosboxFlags, true); count > 1) {
             std::cerr << "Error: You can only use one of the following flags at a time: --dosbox-version, --dosbox-version-manual" << std::endl;
@@ -131,12 +165,32 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // This is post parameter checking, we check if the --replace-dosbox flag is set to true and if so we check if
+    // 1. If --dosbox-version is set, we check if the application is installed in the system, if not we print an error message
+    // 2. If --dosbox-version-manual is set, we check if the path is valid, if not we print an error message
+    if (program["--replace-dosbox"] == true) {
+        if (const auto dosboxArgument = program.get<std::string>("--dosbox-version"); !dosboxArgument.empty()) {
+            if (auto applications = DosboxStagingReplacer::InstallationFinder::findApplication(
+                        dosBoxVersionParameters[dosboxArgument]);
+                applications.empty()) {
+                std::cerr << "Error: The application you specified is not installed in the system" << std::endl;
+                return -1;
+            }
+        } else if (!program.get<std::string>("--dosbox-version-manual").empty()) {
+            if (const auto dosboxPath = program.get<std::string>("--dosbox-version-manual");
+                !DosboxStagingReplacer::fileExists(dosboxPath)) {
+                std::cerr << "Error: The path you specified does not exist" << std::endl;
+                return -1;
+            }
+        }
+    }
+
     // If there are no operation flags set, we do not do anything but print help
     if (operations_count == 1) {
         DosboxStagingReplacer::FileBackupService fileBackupService;
         // We set the values from --file and --directory to variables
-        std::filesystem::path chosenFile = program.get<std::string>("--file");
-        std::filesystem::path chosenPath = program.get<std::string>("--directory");
+        const std::filesystem::path chosenFile = program.get<std::string>("--file");
+        const std::filesystem::path chosenPath = program.get<std::string>("--directory");
         // Initialize a data exporter base on the format chosen by the user
         const auto dataExporter = DosboxStagingReplacer::DataExporterFactory::createDataExporter(program.get<std::string>("--format"));
         // We initialize a vector of FileEntity objects to store the files and use it later with DirectoryScanner
@@ -146,7 +200,7 @@ int main(int argc, char *argv[]) {
         DosboxStagingReplacer::GogGalaxyService service;
 
         try {
-            files = DosboxStagingReplacer::DirectoryScanner::scanDirectory(chosenPath);
+            files = DosboxStagingReplacer::DirectoryScanner::scanDirectory(chosenPath.string());
         } catch (const std::exception &e) {
             std::cerr << "Error: " << e.what() << std::endl;
             return -1;
@@ -160,7 +214,7 @@ int main(int argc, char *argv[]) {
             }
             std::cout << "Creating a backup of " << chosenFile << " in " << chosenPath << std::endl;
             service.closeConnection();
-            fileBackupService.createBackup((chosenPath / chosenFile).string(), files);
+            auto backupFile = fileBackupService.createBackup((chosenPath / chosenFile).string(), files);
         }
         else if (program["--restore"] == true) {
             service.openConnection((chosenPath / chosenFile).string());
@@ -170,10 +224,14 @@ int main(int argc, char *argv[]) {
             }
             std::cout << "Restoring the backup of " << chosenFile << " in " << chosenPath << std::endl;
             service.closeConnection();
-            fileBackupService.restoreFromBackup((chosenPath / chosenFile).string(), files);
+            auto restoredFile = fileBackupService.restoreFromBackup((chosenPath / chosenFile).string(), files);
         }
         else if (program["--list-applications"] == true) {
-            const auto applications = DosboxStagingReplacer::getInstalledApplications();
+            std::vector<DosboxStagingReplacer::InstallationInfo> applications;
+            if (program["--dos-only"] == true)
+                applications = DosboxStagingReplacer::InstallationFinder::findApplication("DOSBox");
+            else
+                applications = DosboxStagingReplacer::getInstalledApplications();
             std::cout << dataExporter->serialize(applications) << std::endl;
         }
         else if (program["--list-games"] == true) {
@@ -181,14 +239,24 @@ int main(int argc, char *argv[]) {
             // take advantage of polymorphism
             std::vector<std::shared_ptr<DosboxStagingReplacer::SqlDataResult>> games;
             service.openConnection((chosenPath / chosenFile).string());
-            for (auto& product : service.getProducts()) {
+            for (auto& product : service.getProducts(program.get<bool>("--dos-only"))) {
                 games.push_back(std::make_shared<DosboxStagingReplacer::ProductDetails>(product));
             }
             std::cout << dataExporter->serialize(games) << std::endl;
             service.closeConnection();
         }
-    } else {
-        std::cout << program << std::endl;
+        else if (program["--show-playtasks"] == true) {
+            std::vector<std::shared_ptr<DosboxStagingReplacer::SqlDataResult>> playTasks;
+            service.openConnection((chosenPath / chosenFile).string());
+            for (auto& playTask : service.getPlayTasksFromGameReleaseKey(releaseKey)) {
+                playTasks.push_back(std::make_shared<DosboxStagingReplacer::PlayTaskInformation>(playTask));
+            }
+            std::cout << dataExporter->serialize(playTasks) << std::endl;
+            service.closeConnection();
+        }
+        else if (program["--replace-dosbox"] == true) {
+            std::cout << "TODO: Replace DOSBox version" << std::endl;
+        }
     }
 
     return 0;
