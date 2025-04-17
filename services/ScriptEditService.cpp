0e652b11e5696c76eaf56bcd6b659d2390a960bf
Richard Louie Orilla <richard.orilla@inriver.com>
2025-04-17 14:32:34 +0800

    Refactor ScriptEditService and add new utility methods.
    Moved methods to ScriptEditService class for better organization. Added `isConfigFileDosboxConfig`, `resolveRelativePathsFromString`, and `disableFullScreenForDosboxConfig` functions to expand functionality. Improved comments, readability, and implemented case-insensitive checks consistently.

--- services/ScriptEditService.cpp
+++ services/ScriptEditService.cpp
@@ -10,7 +10,7 @@
 
 namespace DosboxStagingReplacer {
 
-    static void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    void ScriptEditService::replaceAll(std::string& str, const std::string& from, const std::string& to) {
         if (from.empty()) return; // avoid infinite loop
 
         size_t start_pos = 0;
@@ -20,8 +20,8 @@ namespace DosboxStagingReplacer {
         }
     }
 
-    static bool isConfigFileDosboxAutoExec(const std::filesystem::path &filePath) {
-        // We open the file and check if it contains the string "[autoexec] (case insensitive)"
+    bool ScriptEditService::isConfigFileDosboxAutoExec(const std::filesystem::path &filePath) {
+        // We open the file and check if it contains the string "[autoexec] (case-insensitive)"
         if (std::fstream file(filePath); file.is_open()) {
             std::string line;
             while (std::getline(file, line)) {
@@ -35,7 +35,39 @@ namespace DosboxStagingReplacer {
         return false;
     }
 
-    static void resolveRelativePathsForDosboxAutoExec(std::filesystem::path &filePath, const std::filesystem::path &basePath, const std::string &tmpExtension = ".tmp") {
+    bool ScriptEditService::isConfigFileDosboxConfig(const std::filesystem::path &filePath) {
+        // We open the file and check if it contains the string "[autoexec] (case-insensitive)"
+        if (std::fstream file(filePath); file.is_open()) {
+            std::string line;
+            bool foundSDLFlag = false;
+            bool foundDosboxFlag = false;
+            while (std::getline(file, line)) {
+                auto lowerLine = line;
+                std::ranges::transform(lowerLine, lowerLine.begin(), tolower);
+                if (lowerLine.find("[sdl]") != std::string::npos) {
+                    foundSDLFlag = true;
+                }
+                else if (lowerLine.find("[dosbox]") != std::string::npos) {
+                    foundDosboxFlag = true;
+                }
+            }
+            return foundSDLFlag && foundDosboxFlag;
+        }
+        return false;
+    }
+
+    std::string ScriptEditService::resolveRelativePathsFromString(const std::string &cmd,
+                                                                  const std::filesystem::path &basePath) {
+        // Copy cmd to a new variable
+        std::string result = cmd;
+        // Replace all relative paths with absolute paths
+        replaceAll(result, "..", basePath.string());
+        return result;
+    }
+
+    void ScriptEditService::resolveRelativePathsForDosboxAutoExec(std::filesystem::path &filePath,
+                                                                  const std::filesystem::path &basePath,
+                                                                  const std::string &tmpExtension) {
         // We open two files, the original file and a tmp file
         std::fstream file(filePath);
         std::filesystem::path tmpFilePath = filePath;
@@ -59,9 +91,52 @@ namespace DosboxStagingReplacer {
                 // Check if the line contains a path
                 auto lowerLine = line;
                 std::ranges::transform(lowerLine, lowerLine.begin(), tolower);
-                // Check if the line contains the strings "imgmount" or "mount" (case insensitive)
+                // Check if the line contains the strings "imgmount" or "mount" (case-insensitive)
                 if (lowerLine.find("mount") != std::string::npos || lowerLine.find("imgmount") != std::string::npos) {
                     replaceAll(line, "..", basePath.string());
+                }
+                // Write the modified line to the tmp file
+                tmpFile << line << std::endl;
+            }
+        }
+
+        // Close the files
+        file.close();
+        tmpFile.close();
+        // Remove the original file
+        std::filesystem::remove(filePath);
+        // Rename the tmp file to the original file
+        std::filesystem::rename(tmpFilePath, filePath);
+        // We also remove the tmp file
+        std::filesystem::remove(tmpFilePath);
+    }
+
+    void ScriptEditService::disableFullScreenForDosboxConfig(std::filesystem::path &filePath, const std::string &tmpExtension) {
+        // We open two files, the original file and a tmp file
+        std::fstream file(filePath);
+        std::filesystem::path tmpFilePath = filePath;
+        tmpFilePath += tmpExtension;
+
+        // Double check first if the tmp file exists, if so we do something like .tmp2, tmp3, etc
+        if (fileExists(tmpFilePath.string())) {
+            int counter = 2;
+            while (fileExists(tmpFilePath.string())) {
+                tmpFilePath = filePath;
+                tmpFilePath += tmpExtension + std::to_string(counter);
+                counter++;
+            }
+        }
+        std::fstream tmpFile(tmpFilePath, std::ios::out);
+
+        // We slowly stream the file line by line and replace each line with any modifications
+        if (file.is_open() && tmpFile.is_open()) {
+            std::string line;
+            while (std::getline(file, line)) {
+                // Check if the line contains a path
+                auto lowerLine = line;
+                std::ranges::transform(lowerLine, lowerLine.begin(), tolower);
+                if (lowerLine.find("fullscreen=true") != std::string::npos) {
+                    replaceAll(line, "fullscreen=true", "fullscreen=false");
                 }
                 // Write the modified line to the tmp file
                 tmpFile << line << std::endl;
--- services/ScriptEditService.h
+++ services/ScriptEditService.h
@@ -19,7 +19,23 @@ namespace DosboxStagingReplacer {
          * @param filePath path The absolute path to the file.
          * @return True if the file is a DOSBox autoexec configuration file, false otherwise.
          */
-        static bool isConfigFileDosboxAutoExec(std::filesystem::path filePath);
+        static bool isConfigFileDosboxAutoExec(const std::filesystem::path &filePath);
+        /**
+         * @brief Checks if the given file is a valid DOSBox configuration file.
+         * @param filePath The absolute path to the file to be checked.
+         * @return True if the file contains the required DOSBox configuration sections, false otherwise.
+         */
+        static bool isConfigFileDosboxConfig(const std::filesystem::path &filePath);
+
+
+        /**
+         * @brief Resolves relative paths in a command string using a specified base path.
+         * @param cmd The command string that may contain relative paths.
+         * @param basePath The base path used to resolve relative paths to absolute paths.
+         * @return A new string with all relative paths in the command string resolved to absolute paths.
+         */
+        static std::string resolveRelativePathsFromString(const std::string &cmd,
+                                                          const std::filesystem::path &basePath);
         /**
          * @brief Resolve relative paths in the DOSBox autoexec configuration file.
          * @param filePath The path of the file to resolve relative paths in.
@@ -27,6 +43,13 @@ namespace DosboxStagingReplacer {
          * @param tmpExtension The extension to use for the temporary file.
          */
         static void resolveRelativePathsForDosboxAutoExec(std::filesystem::path &filePath, const std::filesystem::path &basePath, const std::string &tmpExtension = ".tmp");
+
+        /**
+         * @brief Disables fullscreen for the DOSBox configuration file.
+         * @param filePath The path of the DOSBox configuration file to modify.
+         * @param tmpExtension The extension to use for the temporary file
+         */
+        static void disableFullScreenForDosboxConfig(std::filesystem::path &filePath, const std::string &tmpExtension = ".tmp");
     };
 
 } // DosboxStagingReplacer
